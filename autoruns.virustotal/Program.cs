b0fd74d7a511803b3a0e1da9112852cbf6db5663
George J. Capnias <george.capnias@outlook.com>
2022-03-20 14:39:51 +0200

    Initial commit of project

---
+++ autoruns.virustotal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace autoruns.virustotal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string textStream = null;
+
+            if (Console.IsInputRedirected)
+            {
+                textStream = Console.In.ReadToEnd();
+                Console.Out.WriteLine("Console is redirected!\r\n\r\n");
+                Console.Out.WriteLine(textStream);
+            }
+            else
+            {
+                Console.Out.WriteLine("Console is not redirected!\r\n");
+            }
+
+        }
+    }
+}
